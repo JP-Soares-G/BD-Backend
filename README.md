ebd53284eb28f7a0e2713482541a953198330213
JPedro-SG <81583744+JPedro-SG@users.noreply.github.com>
2021-10-18 09:51:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Backend para primeira parte do trabalaho
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.get('/', (req, res) => {
+
+    const produtos = [
+        {
+            id: 1,
+            name: "Camisa",
+            category: "Roupa"
+        },
+        {
+            id: 1283,
+            name: "Notebook",
+            category: "Tecnologia"
+        },
+    ]
+    res.json({produtos})
+})
+
+app.get('/:id', (req, res) => {
+    const camisa = {
+        id: 1,
+        name: 'Camisa',
+        category: 'Roupa',
+        amount: 10,
+        media: 15.56,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates dolorem aliquid quibusdam dolor suscipit deserunt mollitia! Animi error rem maxime aperiam dolores odio minus assumenda, asperiores suscipit ducimus aliquid. Assumenda repellendus quam veniam? Atque in omnis cum perspiciatis id hic aut reiciendis, esse illum corrupti nostrum maiores, temporibus quae cupiditate blanditiis doloribus eum sed! Molestiae perspiciatis totam sapiente cum.'
+    }
+
+    const notebook = {
+        id: 1283,
+        name: 'Notebook',
+        category: 'Tecnologia',
+        amount: 25,
+        media: 1300.56,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates dolorem aliquid quibusdam dolor suscipit deserunt mollitia! Animi error rem maxime aperiam dolores odio minus assumenda, asperiores suscipit ducimus aliquid. Assumenda repellendus quam veniam? Atque in omnis cum perspiciatis id hic aut reiciendis, esse illum corrupti nostrum maiores, temporibus quae cupiditate blanditiis doloribus eum sed! Molestiae perspiciatis totam sapiente cum.'
+    }
+
+    if(req.params.id === '1') return res.json(camisa)
+    res.json(notebook)
+})
+
+const port = 3000
+
+app.listen(port, console.log('Backend is running at port ' + port))
